3da3729bbfcad74833ef70959a645ad6d53bd89a
exagger8 <46694136+exagger8@users.noreply.github.com>
2020-02-17 21:55:43 -0800

    Update Extensions.kt

--- library/src/main/java/pl/com/hop/components/Extensions.kt
+++ library/src/main/java/pl/com/hop/components/Extensions.kt
@@ -14,31 +14,17 @@ val Int.dpToPx: Float get() = (this * Resources.getSystem().displayMetrics.densi
  */
 val Float.pxToDp: Int get() = (this / Resources.getSystem().displayMetrics.density).toInt()
 
-fun Resources.getDefaultStrokeWidth(): Float {
-    return getDimension(R.dimen.default_stroke_width)
-}
+fun Resources.getDefaultStrokeWidth(): Float = getDimension(R.dimen.default_stroke_width)
 
-fun Resources.getDefaultColor(): Int {
-    return if (Build.VERSION.SDK_INT >= 23) {
-        getColor(R.color.default_primary, null)
-    } else {
-        getColor(R.color.default_primary)
-    }
-}
+fun Resources.getDefaultColor(): Int = if (Build.VERSION.SDK_INT >= 23) getColor(R.color.default_primary, null) else getColor(R.color.default_primary)
 
-fun Resources.getDefaultSecondaryColor(): Int {
-    return if (Build.VERSION.SDK_INT >= 23) {
-        getColor(R.color.default_secondary, null)
-    } else {
-        getColor(R.color.default_secondary)
-    }
-}
+fun Resources.getDefaultSecondaryColor(): Int = if (Build.VERSION.SDK_INT >= 23) getColor(R.color.default_secondary, null) else getColor(R.color.default_secondary)
 
-fun Paint.initLinePaint(lineWidth: Float, color: Int) {
-    this.color = color
-    this.style = Paint.Style.STROKE
-    this.strokeWidth = lineWidth
-    this.isDither = true
-    this.strokeJoin = Paint.Join.ROUND
-    this.strokeCap = Paint.Cap.ROUND
+fun Paint.initLinePaint(lineWidth: Float, lineColor: Int) = apply {
+    color = lineColor
+    style = Paint.Style.STROKE
+    strokeWidth = lineWidth
+    isDither = true
+    strokeJoin = Paint.Join.ROUND
+    strokeCap = Paint.Cap.ROUND
 }
